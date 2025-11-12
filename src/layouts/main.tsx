5bb60c5c876474d2c07d22b562685de73f6fcdfc
Lorenzo-Fiori <l.fiori@inxpire.it>
2025-11-12 09:33:51 +0100

    update

--- src/layouts/main.tsx
+++ src/layouts/main.tsx
@@ -73,7 +73,7 @@ const globalClass = css`
 
 const Layout: FC = (props: PropsWithChildren<Props>) => {
   return (
-    <html>
+    <html lang="en">
       <head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <title>{props.title}</title>
